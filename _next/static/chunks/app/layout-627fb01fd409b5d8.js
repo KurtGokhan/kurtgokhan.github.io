d8d3a946c1a03912e7ea82a862d5b55115d34362
KurtGokhan <krtgkn@gmail.com>
2024-03-31 02:15:12 +0000

    Deploying to main from @ KurtGokhan/website@cfcdb9c7c33112f4545f8f5067c021918a533322 🚀

---
+++ _next/static/chunks/app/layout-627fb01fd409b5d8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1559:function(n,e,u){Promise.resolve().then(u.t.bind(u,2445,23))},2445:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=1559)}),_N_E=n.O()}]);
